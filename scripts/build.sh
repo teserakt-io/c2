d739ee2ad1fe3cc6bcd7178fd01ade2388c1ff26
Antony Vennard <antony@vennard.ch>
2018-11-29 11:28:16 +0100

    Fix build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,42 +11,7 @@ if ! [ -x "$(command -v goimports)" ]; then
     exit 1
 fi
 
-APIREPO=
-for GOSRC in ${GOPATH//:/ }; do
-    if [ -d $GOSRC/src/teserakt/backend/cmd ]; then
-        goimports -w $GOSRC/src/teserakt/backend/cmd
-    fi
-
-    # locate the backend api directory
-    if [ -d $GOSRC/src/gitlab.com/teserakt/backend-api ]; then
-        APIREPO=$GOSRC/src/gitlab.com/teserakt/backend-api
-    fi
-done
-
-if [[ -z "$APIREPO" ]]; then 
-    echo "API repository not found on the GOPATH. Have you cloned the"
-    echo "repositories into a gopath location (or symlinked them there?)"
-    exit 1
-fi
-
-# README: If you are wondering why we are doing this, there is a simple reason
-# - golang does not like a package using grpc and a vendored build also using 
-# it. Import paths get confused and builds fail. Consequently we have to 
-# compile the grpc generated code internally in this project, but we keep it 
-# separate as it is shared across multiple projects.
-# This code locates the generated protobuf file and copies it locally.
-PROTOBUFSRC=$APIREPO/pkg/c2proto/c2.pb.go
-echo $PROTOBUFSRC
-PROTOBUFDST=pkg/c2proto
-if [ ! -f $PROTOBUFSRC ]; then
-    echo "protobuf go code not generated. Please check the repository";
-    exit 1
-fi
-
-mkdir -p $PROTOBUFDST
-cp $PROTOBUFSRC $PROTOBUFDST
-
-CMDPATH=gitlab.com/teserakt/backend/cmd
+goimports -w cmd/c2backend
 
 if [[ -z "$E4_GIT_COMMIT" ]]; then 
     if [[ ! -x "$(command -v git)" ]]; then 
@@ -64,7 +29,6 @@ fi
 NOW=$(date "+%Y%m%d")
 
 
-
 # see valid values at https://gist.github.com/asukakenji/f15ba7e588ac42795f421b48b8aede63
 #GOOS=linux 
 
@@ -74,4 +38,4 @@ GOARCH=amd64
 printf "building $PROJECT:\n\tversion $NOW-$GIT_COMMIT\n\tOS $GOOS\n\tarch: $GOARCH\n"
 
 printf "=> $PROJECT...\n"
-GOOS=$GOOS GOARCH=$GOARCH go build -o bin/c2backend -ldflags "-X main.gitCommit=$GIT_COMMIT -X main.buildDate=$NOW" $CMDPATH/c2backend 
+GOOS=$GOOS GOARCH=$GOARCH go build -o bin/c2backend -ldflags "-X main.gitCommit=$GIT_COMMIT -X main.buildDate=$NOW" ${PWD}/cmd/c2backend 
