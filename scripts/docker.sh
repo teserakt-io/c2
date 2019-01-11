a1480b1da7a88786b579b9178742a32804e534b3
Antony Vennard <antony@vennard.ch>
2019-01-11 10:51:19 +0100

    Re-add support for building the c2backend as a docker image.

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -3,6 +3,8 @@
 echo "c2backend Docker build script (c) Teserakt AG 2018. All Right Reserved"
 echo ""
 
+GOOS=linux GOARCH=amd64 ./scripts/xbuild.sh
+
 E4_VERSION=""
 E4_GIT_COMMIT=$(git rev-list -1 HEAD)
 
@@ -11,7 +13,6 @@ if [[ -z "$BUILDVERSION" ]]; then
 fi
 
 echo "Building version $E4_VERSION, commit $E4_GIT_COMMIT\n"
-
 
 printf "=> backend"
 sudo -E docker build --target c2 -t "e4/c2backend:$E4_VERSION" -t "e4/c2backend:$E4_GIT_COMMIT" -f docker/Dockerfile.c2 .
---
+++ scripts/xbuild.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+PROJECT=c2backend
+
+echo "$PROJECT build script (c) Teserakt AG 2018. All rights reserved."
+echo ""
+
+goimports -w cmd/$PROJECT
+
+GIT_COMMIT=$(git rev-list -1 HEAD)
+NOW=$(date "+%Y%m%d")
+
+if [ -z "$GOOS" ]; then 
+    GOOS=`uname -s | tr '[:upper:]' '[:lower:]'` 
+fi
+if [ -z "$GOARCH" ]; then
+    GOARCH=amd64
+fi
+
+printf "building $PROJECT:\n\tversion $NOW-$GIT_COMMIT\n\tOS $GOOS\n\tarch: $GOARCH\n"
+
+printf "=> $PROJECT...\n"
+GOOS=$GOOS GOARCH=$GOARCH go build -o bin/$PROJECT.$GOOS.$GOARCH -ldflags "-X main.gitCommit=$GIT_COMMIT -X main.buildDate=$NOW" ${PWD}/cmd/$PROJECT
