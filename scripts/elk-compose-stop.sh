c16e2bae05d8f53af2ca6b609e6aa658fbd50657
JP Aumasson <jeanphilippe.aumasson@gmail.com>
2019-02-11 17:44:52 +0100

    add logstash to stop script

--- scripts/elk-compose-stop.sh
+++ scripts/elk-compose-stop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker stop e4-kibana e4-elasticsearch
-docker rm e4-kibana e4-elasticsearch
+docker stop e4-kibana e4-elasticsearch e4-logstash
+docker rm e4-kibana e4-elasticsearch e4-logstash
