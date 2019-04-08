148f7d66c233fdc1de9a64964cc3b81bfe2a59dd
JP Aumasson <jeanphilippe.aumasson@gmail.com>
2019-04-08 20:15:42 +0200

    get version from rev-parse

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,13 +7,13 @@ echo ""
 
 goimports -w cmd/$PROJECT
 
-GIT_COMMIT=$(git rev-list -1 HEAD)
+GIT_COMMIT=$(git rev-parse --short HEAD)
 NOW=$(date "+%Y%m%d")
 
 GOOS=`uname -s | tr '[:upper:]' '[:lower:]'` 
 GOARCH=amd64
 
-printf "building $PROJECT:\n\tversion $NOW-$GIT_COMMIT\n\tOS $GOOS\n\tarch: $GOARCH\n"
+printf "building $PROJECT:\n\tversion:\t$NOW-$GIT_COMMIT\n\tOS:\t\t$GOOS\n\tarch:\t\t$GOARCH\n"
 
 printf "=> $PROJECT...\n"
 GOOS=$GOOS GOARCH=$GOARCH go build -o bin/$PROJECT -ldflags "-X main.gitCommit=$GIT_COMMIT -X main.buildDate=$NOW" ${PWD}/cmd/$PROJECT
