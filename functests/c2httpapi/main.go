96a88aa0a141acdd8010ce015717e422e6888814
Antony Vennard <antony@vennard.ch>
2019-04-11 10:14:48 +0200

    Use Teserakt's own public broker for functests.

--- functests/c2httpapi/main.go
+++ functests/c2httpapi/main.go
@@ -46,11 +46,13 @@ func main() {
 	}
 
 	DBNAME := fmt.Sprintf("E4C2_DB_FILE=%s", e4test.GetRandomDBName())
+	BROKER := "E4C2_MQTT_BROKER=tcp://mqtt.teserakt.io:1883"
 
 	fmt.Fprintf(os.Stderr, "Database set to %s\n", DBNAME)
 
 	env := []string{"E4C2_DB_TYPE=sqlite3"}
 	env = append(env, DBNAME)
+	env = append(env, BROKER)
 
 	go e4test.RunDaemon(errc, stopc, waitdrunc, c2binary, []string{}, env, func() bool {
 		return e4test.CheckC2Online(timeoutSeconds*time.Second, 5555, 8888, "127.0.0.1")
