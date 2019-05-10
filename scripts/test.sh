174ca3059c486e55027ea031082e3ec74511c82b
Antony Vennard <antony@vennard.ch>
2019-05-10 10:30:54 +0200

    Editorconfig => everyone uses tabs, have your own indent. Don't set root=true in it.
    Test and unittests.sh use the race detector.

--- scripts/test.sh
+++ scripts/test.sh
@@ -11,8 +11,8 @@ FUNCPATH=`pwd`/functests
 GOOS=`uname -s | tr '[:upper:]' '[:lower:]'`
 GOARCH=amd64
 
-GOOS=$GOOS GOARCH=$GOARCH go build -o test/bin/c2httpapi -ldflags "-X main.gitCommit=$GIT_COMMIT -X main.buildDate=$NOW" $FUNCPATH/c2httpapi
-GOOS=$GOOS GOARCH=$GOARCH go build -o test/bin/c2grpcapi -ldflags "-X main.gitCommit=$GIT_COMMIT -X main.buildDate=$NOW" $FUNCPATH/c2grpcapi
+GOOS=$GOOS GOARCH=$GOARCH go build -o test/bin/c2httpapi -race -ldflags "-X main.gitCommit=$GIT_COMMIT -X main.buildDate=$NOW" $FUNCPATH/c2httpapi
+GOOS=$GOOS GOARCH=$GOARCH go build -o test/bin/c2grpcapi -race -ldflags "-X main.gitCommit=$GIT_COMMIT -X main.buildDate=$NOW" $FUNCPATH/c2grpcapi
 
 ./test/bin/c2httpapi
 ./test/bin/c2grpcapi
